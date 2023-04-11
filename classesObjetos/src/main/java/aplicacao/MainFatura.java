4bbb28bd3b81eaadb82669bd91109ad63f085b34
emanuelly-portela <emanuellyportela@gmail.com>
2023-04-10 22:04:30 -0300

    Renomear classe

--- classesObjetos/src/main/java/aplicacao/MainFatura.java
+++ classesObjetos/src/main/java/aplicacao/MainFatura.java
@@ -3,7 +3,7 @@ package aplicacao;
 import classes.Fatura;
 import java.util.Scanner;
 
-public class main {
+public class MainFatura {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
